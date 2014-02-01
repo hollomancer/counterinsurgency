3977f38408f178a8ca9e11bae6a8bb0c35f58056
hollomancer_home <chollomon@gmail.com>
2014-02-01 10:09:50 -0500

    some thoughts on how to consolidate global game states

---
+++ data/globals.lua
@@ -0,0 +1,5 @@
+--# place to consolidate global game states
+
+current_turn
+game_end_turn
+chaos: this number adds a degree of randomness to particularly critical actions. conditions in the world can affect the level of chaos. All of your VITALS counteract chaos.
