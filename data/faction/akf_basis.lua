839d2155e61a25d0d6c77dcd1ae53dcd9fc7de27
hollomancer_laptop <chollomon@gmail.com>
2014-02-01 22:14:10 -0500

    Updating README with synopsis added stubs for Basis faction added a location stub

---
+++ data/faction/akf_basis.lua
@@ -0,0 +1,21 @@
+FACTION (AKF): THE BASIS
+
+Suit: Spades
+
+Extremists who only want to cause as much chaos as possible.
+
+Characters with Militant personalities contribute 20% more power to this faction.
+Characters with Deceptive personalities contribute 10% more power to this faction.
+Characters with Cautious personalities contribute 10% more power to this faction.
+Characters with Pragmatic personalities contribute 10% more power to this faction.
+
+BASIS insurgents are difficult to gain intelligence on. To open them up for direct attack, you must;
+
+	* Successfully reduce the power of one of the three main Insurgent factions enough that a BASIS leader can be targeted by intelligence. Capturing the BASIS HVT will open up the BASIS faction for targeting.
+	* Capture a BASIS agent during one of their attacks - they are resistant to interrogation skill and impervious to negotation skill.
+
+BASIS does not strike often, but they do strike spectacularly when they do. Expect at least one or two extremely deadly attacks by BASIS. A successful BASIS attack increases global Chaos twice as much as a regular terror attack.
+
+Any power that a character has with an individual AKF faction, also goes to BASIS.
+
+Victories against BASIS have a significant effect on your score.
---
+++ data/locations/loc_s2.lua
@@ -0,0 +1,3 @@
+LOCATION: Battalion Intelligence
+
+The Intelligence Summary is a good way for you to get an idea of what's going on in greater Khyberia, including upcoming holidays and events that may affect your missions.
