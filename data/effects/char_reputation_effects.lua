f9062f3334ca187b21f486b0290df04cb7ff8d20
hollomancer_home <chollomon@gmail.com>
2014-02-09 14:53:35 -0500

    reformatting civil development notes, and reputation effects appropriately work still needs to be done to finish these

---
+++ data/effects/char_reputation_effects.lua
@@ -0,0 +1,78 @@
+--# char_reputation_effects.lua
+--# Base data for all character reputation effects.
+
+char_reputation_effects = {
+  
+  base = {
+    name = "PLACEHOLDER",
+    desc_short = "PLACEHOLDER",
+    desc_long = "PLACEHOLDER",
+  },
+  
+  e_wealth = {
+    name = "e_wealth"
+    desc_short = "Wealth"
+  },
+
+  e_charismatic = {
+    name = "e_charismatic"
+    desc_short = "Charismatic"
+  },
+
+  e_vet = {
+    name = "e_vet"
+    desc_short = "Veteran"
+  },
+
+  e_hero = {
+    name = "e_hero"
+    desc_short = "War Hero"
+  },
+
+  e_legend = {
+    name = "e_legend"
+    desc_short = "Legend"
+  },
+
+  e_dynasticpower = {
+    name = "e_dynasticpower"
+    desc_short = "Dynastic Power"
+  },
+
+  e_effective = {
+    name = "e_effective"
+    desc_short = "Effective"
+  },
+
+  e_effective = {
+    name = "e_ineffective"
+    desc_short = "Ineffective"
+  },
+
+  e_bluefalcon = {
+    name = "e_bluefalcon"
+    desc_short = "Blue Falcon"
+    desc_long = "Buddy is only half the word..."
+  },
+
+  e_popular = {
+    name = "e_popular"
+    desc_short = "Popular"
+  },
+
+  e_wellliked = {
+    name = "e_wellliked"
+    desc_short = "Well Liked"
+  },
+
+  e_disliked = {
+    name = "e_disliked"
+    desc_short = "Disliked"
+  },
+
+  e_anathema = {
+    name = "e_anathema"
+    desc_short = "Anathema"
+  },
+  
+}
