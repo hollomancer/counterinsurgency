3dcae4c9c8a5695f3e309035c6b373f6c00c307d
hollomancer <chollomon@gmail.com>
2014-02-09 19:31:30 -0500

    add team effects such as ethos

---
+++ data/effects/team_ethos.lua
@@ -0,0 +1,23 @@
+-- team_ethos.lua
+
+team_ethos = {
+
+  team_war_ethos = {
+    desc_short = "Warrior Ethos",
+    desc_long = "Your team is aggressive, energetic, and ready to engage and destroy the enemy.",
+    VIG = 1,
+    INT = -1,
+    priority = 3,
+    -- My bayonet, on the wings of power, keeps the peace worldwide.
+    },
+
+  team_hum_ethos = {
+    desc_short = "Humanitarian Ethos",
+    desc_long = "Your team is patient, contemplative, and ready to win hearts and minds.",
+    INT = 1,
+    VIG = -1,
+    priority = 3,
+    -- Some of the best weapons for counterinsurgents do not shoot.
+  },
+
+}
